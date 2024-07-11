92983683cabc2c3ffaec31d42bc764cf09f495fd
joyal-jij0 <joyal2405@gmail.com>
2024-07-11 11:05:49 +0530

    Completed Project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import React from "react";
+
+import { Router } from "./routes/Router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const App = () => {
+  return <Router />;
+};
+
+export default App;
